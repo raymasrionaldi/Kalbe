f2db29b110b996d40db372222878954b21b6b3de
Dipatria <johandi.patria@gmail.com>
2019-10-31 15:31:58 +0700

    Membuat utils ambil inisial
    menambahkan dua fungsi untuk ambil inisial

---
+++ app/src/main/java/com/xsis/android/batch217/utils/AmbilInisial.kt
@@ -0,0 +1,29 @@
+package com.xsis.android.batch217.utils
+
+fun ambilDuaInisial(nama: String): String{
+    var inisial = ""
+    if (nama.isNotBlank()){
+        val arrayKata = nama.split(" ")
+        for(i in 0 until arrayKata.size){
+            if (i > 1 && !arrayKata[i][0].isLetter()){
+                break
+            }
+            inisial += "${arrayKata[i][0]}".toUpperCase()
+        }
+    }
+    return inisial
+}
+
+fun ambilSemuaInisial(nama: String): String{
+    var inisial = ""
+    if (nama.isNotBlank()){
+        val arrayKata = nama.split(" ")
+        for(i in 0 until arrayKata.size){
+            if (!arrayKata[i][0].isLetter()){
+                break
+            }
+            inisial += "${arrayKata[i][0]}".toUpperCase()
+        }
+    }
+    return inisial
+}
