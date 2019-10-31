60c0b42fa83bdbf146908fcb9bf3efd09c6ceec3
bulandg <bulandewi.g@gmail.com>
2019-10-31 12:00:57 +0700

    Init Project
    Source Code Mini Project Awal

---
+++ app/src/main/java/com/xsis/android/batch217/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.xsis.android.batch217
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
