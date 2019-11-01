bdf1df502fe95774545732e2afbfb867d302562e
bulandg <bulandewi.g@gmail.com>
2019-11-01 14:11:07 +0700

    ADD utils/ConstantDb
    add konstanta variabel database

---
+++ app/src/main/java/com/xsis/android/batch217/utils/ConstantDb.kt
@@ -0,0 +1,113 @@
+package com.xsis.android.batch217.utils
+
+/*=================================================*/
+/*                  KONSTANTA DATABASE             */
+/*=================================================*/
+val TABEL_AGAMA ="agama"
+val ID_AGAMA="id_agama"
+val NAMA_AGAMA="nama_agama"
+val DES_AGAMA="des_agama"
+
+val TABEL_BACKOFFICE_POSITION ="backoffice_position"
+val ID_BACKOFFICE="id_backoffice"
+val NAMA_BACKOFFICE="nama_backoffice"
+val LEVEL_BACKOFFICE="level_backoffice"
+val NOTE_BACKOFFICE="note_backoffice"
+
+val TABEL_COMPANY ="company"
+val ID_COMPANY="id_company"
+val NAMA_COMPANY="nama_company"
+val KOTA_COMPANY="kota_company"
+val KDPOS_COMPANY="kdpos_company"
+val JLN_COMPANY="jln_company"
+val BUILDING_COMPANY="building_company"
+val FLOOR_COMPANY="building_company"
+
+val TABEL_POSITION_LEVEL ="position_level"
+val ID_POSITION="id_position"
+val NAMA_POSITION="nama_position"
+val DES_POSITION="des_position"
+
+val TABEL_CONTRACT_STATUS = "contract_status"
+val ID_CONTRACT="id_contract"
+val NAMA_CONTRACT = "nama_contract"
+val DES_CONTRACT = "des_contract"
+
+val TABEL_EMPLOYEE_STATUS="employee_status"
+val ID_EMP="id_emp"
+val NAMA_EMP = "nama_emp"
+val DES_EMP = "des_emp"
+
+val TABEL_EMPLOYEE_TYPE = "employee_type"
+val ID_EMP_TYPE = "id_emp_type"
+val NAMA_EMP_TYPE = "nama_emp_type"
+val DES_EMP_TYPE= "des_emp_type"
+
+val TABEL_GRADE= "grade"
+val ID_GRADE="id_grade"
+val NAMA_GRADE="nama_grade"
+val DES_GRADE="des_grade"
+
+val TABEL_CATATAN = "jenis_catatan"
+val ID_CATATAN= "id_catatan"
+val NAMA_CATATAN ="nama_catatan"
+val DES_CATATAN = "des_catatan"
+
+val TABEL_KEAHLIAN= "keahlian"
+val ID_KEAHLIAN = "id_keahlian"
+val NAMA_KEAHLIAN= "nama_keahlian"
+val DES_KEAHLIAN="des_keahlian"
+
+val TABEL_PENDIDIKAN= "pendidikan"
+val ID_PENDIDIKAN = "id_pendidikan"
+val NAMA_PENDIDIKAN= "nama_pendidikan"
+val DES_PENDIDIKAN= "des_pendidikan"
+
+val TABEL_PERIODE="periode"
+val ID_PERIODE="id_periode"
+val NAMA_PERIODE="nama_periode"
+val DES_PERIODE="des_periode"
+
+val TABEL_POSITION_LVL = "position_level"
+val ID_POSITION_LVL ="id_position"
+val NAMA_POSITION_LVL= "nama_position"
+val DES_POSITION_LVL="des_position"
+
+val TABEL_PRF="prf_status"
+val ID_PRF="id_prf"
+val NAMA_PRF="nama_prf"
+val DES_PRF="des_prf"
+
+val TABEL_PROVIDER = "provider_tools"
+val ID_PROVIDER="id_provider"
+val NAMA_PROVIDER="nama_provider"
+val DES_PROVIDER="des_provider"
+
+val TABEL_STATUS_NIKAH="status_pernikahan"
+val ID_STATUS_NIKAH="id_status"
+val NAMA_STATUS_NIKAH="nama_status"
+val DES_STATUS_NIKAH="des_status"
+
+val TABEL_TIPE_IDENTITAS ="tipe_identitas"
+val ID_IDENTITAS="id_identitas"
+val NAMA_IDENTITAS="nama_identitas"
+val DES_IDENTITAS="des_identitas"
+
+val TABEL_TIPE_TES="tipe_tes"
+val ID_TES="id_tes"
+val NAMA_TES="nama_tes"
+val DES_TES="des_tes"
+
+val TABEL_TRAINING= "training"
+val ID_TRAINING="id_training"
+val NAMA_TRAINING="nama_training"
+val DES_TRAINING="des_training"
+
+val TABEL_TRAINING_ORG="training_organizer"
+val ID_TRAINING_ORG="id_to"
+val NAMA_TRAINING_ORG="nama_to"
+val DES_TRAINING_ORG="des_to"
+
+//nama field sama
+val IS_DELETED= "is_deleted"
+/*=================================================*/
