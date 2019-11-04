21bb0f79ded96e583bfb30a0bc84f5f7888c7b09
bulandg <bulandewi.g@gmail.com>
2019-11-04 11:56:56 +0700

    FIX BUG double search icon
    UPDATE HomeActivity ->
    comment onCreateOptionsMenu
    to avoid double search icon

--- app/src/main/java/com/xsis/android/batch217/HomeActivity.kt
+++ app/src/main/java/com/xsis/android/batch217/HomeActivity.kt
@@ -42,11 +42,11 @@ class HomeActivity : AppCompatActivity() {
         databaseHelper.createDatabaseFromImportedFile()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }
+    }*/
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
