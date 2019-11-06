0881d72e8ad34ef47b34b82ecff52d2c8d6d1603
bcandraas <bcandraas@gmail.com>
2019-11-06 15:38:18 +0700

    employee type

---
+++ app/src/main/java/com/xsis/android/batch217/models/EmployeeType.kt
@@ -0,0 +1,8 @@
+package com.xsis.android.batch217.models
+
+class EmployeeType {
+    var id_emp_type:Int= 0
+    var nama_emp_type:String?=null
+    var des_emp_type:String? =null
+    var is_Deleted:String?=null
+}
--- app/src/main/java/com/xsis/android/batch217/ui/jenjang_pendidikan/InputPendidikanActivity.kt
+++ app/src/main/java/com/xsis/android/batch217/ui/jenjang_pendidikan/InputPendidikanActivity.kt
@@ -43,7 +43,7 @@ class InputPendidikanActivity : AppCompatActivity() {
         batal()
 
         btnNavDrawer.setOnClickListener {
-            batal()
+            finish()
         }
     }
 
