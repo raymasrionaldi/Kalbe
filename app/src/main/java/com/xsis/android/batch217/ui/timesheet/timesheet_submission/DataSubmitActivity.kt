36edc5c3d0239c0e7c1bc458ad2f6a83346364e1
natuza <57204719+natuza@users.noreply.github.com>
2019-11-14 02:22:50 -0800

    update timesheet_sub send email dan tentuin tanggal min max

--- app/src/main/java/com/xsis/android/batch217/ui/timesheet/timesheet_submission/DataSubmitActivity.kt
+++ app/src/main/java/com/xsis/android/batch217/ui/timesheet/timesheet_submission/DataSubmitActivity.kt
@@ -2,35 +2,46 @@ package com.xsis.android.batch217.ui.timesheet.timesheet_submission
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.xsis.android.batch217.R
-import com.xsis.android.batch217.adapters.ListKeahlianAdapter
 import com.xsis.android.batch217.adapters.ListTimSubAdapter
-import com.xsis.android.batch217.adapters.ListTimesheetApprovalAdapter
-import com.xsis.android.batch217.databases.CompanyQueryHelper
 import com.xsis.android.batch217.databases.DatabaseHelper
-import com.xsis.android.batch217.databases.KeahlianQueryHelper
 import com.xsis.android.batch217.databases.TimesheetQueryHelper
-import com.xsis.android.batch217.models.Keahlian
 import com.xsis.android.batch217.models.Timesheet
-import com.xsis.android.batch217.ui.keahlian.KeahlianViewModel
 import com.xsis.android.batch217.utils.*
 import kotlinx.android.synthetic.main.activity_data_submit.*
-import kotlinx.android.synthetic.main.activity_timesheet_approval_process.*
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.mail.Authenticator
+import javax.mail.Message
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
 
 class DataSubmitActivity : AppCompatActivity() {
 
     val context = this
-    lateinit var listTimesheet : List<Timesheet>
-  //  var databaseQueryHelper: TimesheetQueryHelper? = null
+    lateinit var listTimesheet: List<Timesheet>
+
+    val user = "fajrixiomi@gmail.com"
+    val tos = arrayOf("molenfajri@gmail.com")
+    val ccs = arrayOf<String>()
+    val title = "Data Submission"
+    //var body = ""
+    val password = "setiawan001"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,9 +52,11 @@ class DataSubmitActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.activity_data_submit)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
         try {
             this.supportActionBar!!.hide()
-        } catch (e: NullPointerException){
+        } catch (e: NullPointerException) {
         }
 
         var year = ""
@@ -57,7 +70,8 @@ class DataSubmitActivity : AppCompatActivity() {
         val databaseHelper = DatabaseHelper(context)
         val databaseQueryHelper = TimesheetQueryHelper(databaseHelper)
 
-        listTimesheet = databaseQueryHelper.getTimesheetBerdasarkanWaktuDanProgress(month, year, CREATED)
+        listTimesheet =
+            databaseQueryHelper.getTimesheetBerdasarkanWaktuDanProgress(month, year, CREATED)
 
         if (listTimesheet.isEmpty() || year.isEmpty() || month.isEmpty()) {
             dataNotFoundTimSub.visibility = View.VISIBLE
@@ -78,43 +92,42 @@ class DataSubmitActivity : AppCompatActivity() {
             listTimSubRecycler.adapter = adapterTimSub
             adapterTimSub.notifyDataSetChanged()
 
-            buttonNextSub.setOnClickListener{
+            buttonNextSub.setOnClickListener {
                 submitted()
             }
 
         }
 
-        buttonBackTimeSub.setOnClickListener{
+        buttonBackTimeSub.setOnClickListener {
             finish()
         }
 
     }
 
 
-
-    fun submitted(){
+    fun submitted() {
         val konfirmasiSubmit = AlertDialog.Builder(context)
         konfirmasiSubmit.setMessage("DATA SUBMISSION\n\nAre you sure to submit the data ?\n\n\n")
-            .setPositiveButton("SUBMIT", DialogInterface.OnClickListener{ dialog, which ->
-                Toast.makeText(context,"SUKSES!!!", Toast.LENGTH_SHORT).show()
-//
-//                val databaseHelper = DatabaseHelper(context)
-//                val db = databaseHelper.writableDatabase
-//                val queryDelete = "UPDATE $TABEL_TIPE_IDENTITAS SET $IS_DELETED = 'true' WHERE $ID_IDENTITAS = $ID"
-//                db.execSQL(queryDelete)
-//                fragment.search2()
+            .setPositiveButton("SUBMIT", DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(context, "SUKSES!!!", Toast.LENGTH_SHORT).show()
+
                 val databaseHelper = DatabaseHelper(context!!)
                 val databaseQueryHelper = TimesheetQueryHelper(databaseHelper)
                 val db = databaseHelper.writableDatabase
-
-                for(timesheet in listTimesheet) {
+                //ubah status created to submitted
+                for (timesheet in listTimesheet) {
                     val queryProgress =
                         "UPDATE $TABEL_TIMESHEET SET $PROGRESS_TIMESHEET = 'Submitted' WHERE $ID_TIMESHEET = ${timesheet.id_timesheet}"
                     db.execSQL(queryProgress)
                 }
+
+                //send to email
+                sendEmail(user, tos, ccs, title, password)
+
+                //pesan berhasil disubmit
                 val berhasilSubmited = AlertDialog.Builder(context)
                 berhasilSubmited.setMessage("DATA HAS BEEN SUBMISSION\n\n")
-                    .setPositiveButton("CLOSE", DialogInterface.OnClickListener{ dialog, which ->
+                    .setPositiveButton("CLOSE", DialogInterface.OnClickListener { dialog, which ->
                         dialog.cancel()
                         finish()
                     })
@@ -122,12 +135,73 @@ class DataSubmitActivity : AppCompatActivity() {
 
                 berhasilSubmited.create().show()
 
+
             })
-            .setNegativeButton("CANCEL", DialogInterface.OnClickListener{ dialog, which ->
+            .setNegativeButton("CANCEL", DialogInterface.OnClickListener { dialog, which ->
                 dialog.cancel()
             })
             .setCancelable(true)
 
         konfirmasiSubmit.create().show()
+    }
+
+    //Kirim Email
+    val auth = object : Authenticator() {
+        override fun getPasswordAuthentication(): javax.mail.PasswordAuthentication? =
+            PasswordAuthentication(user, password.toString())
+    }
+
+    private fun sendEmail(
+        user: String, tos: Array<String>, ccs: Array<String>, title: String,
+        password: String
+    ) {
+        //fungsi tampilin tanggal min max
+        val formatter1 = SimpleDateFormat(DATE_PATTERN)
+        val formatter2 = SimpleDateFormat("dd-MMMM-yyyy")
+        var min = Long.MAX_VALUE
+        var max = Long.MIN_VALUE
+        for (timesheet in listTimesheet) {
+            val dateString = formatter1.parse(timesheet.reportDate_timesheet).time
+            if (dateString > max) {
+                max = dateString
+            }
+            if (dateString < min) {
+                min = dateString
+            }
+        }
+
+        val body = "Timesheet tanggal ${formatter2.format(min)} s/d ${formatter2.format(max)}"
+
+
+        //fungsi send mail
+        val props = Properties()
+        val host = "smtp.gmail.com"
+        with(props) {
+            put("mail.smtp.host", host)
+            put("mail.smtp.port", "587") // for TLS
+            put("mail.smtp.auth", "true")
+            put("mail.smtp.starttls.enable", "true")
+        }
+
+        val session = Session.getInstance(
+            props,
+            auth
+        )
+        val message = MimeMessage(session)
+        with(message) {
+            setFrom(InternetAddress(user))
+            for (to in tos) addRecipient(Message.RecipientType.TO, InternetAddress(to))
+            for (cc in ccs) addRecipient(Message.RecipientType.TO, InternetAddress(cc))
+            subject = title
+            setText(body)
+
+        }
+        val transport = session.getTransport("smtp")
+        with(transport) {
+            connect(host, user, password)
+            sendMessage(message, message.allRecipients)
+            close()
+        }
+
     }
 }
