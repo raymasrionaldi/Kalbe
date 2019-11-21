d476f86255b6af8809cbe3ade93ad3dd1fc0b8cd
hanifan-94 <satriahanif4@gmail.com>
2019-11-21 16:32:05 +0700

    back office position dkk

---
+++ app/src/main/java/com/xsis/android/batch217/ui/back_office_position/BackOfficeFragmentForm.kt
@@ -0,0 +1,8 @@
+package com.xsis.android.batch217.ui.back_office_position
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+class BackOfficeFragmentForm(context: Context, val fm: FragmentManager) : Fragment() {
+}
