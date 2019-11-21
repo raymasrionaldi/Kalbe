c07c581cabbca7e0c63d64b0b793ad4030a27102
hanifan-94 <satriahanif4@gmail.com>
2019-11-21 15:26:49 +0700

    update back office

---
+++ app/src/main/java/com/xsis/android/batch217/adapters/fragments/BackOfficeFragmentAdapter.kt
@@ -0,0 +1,17 @@
+package com.xsis.android.batch217.adapters.fragments
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class BackOfficeFragmentAdapter (val context: Context, val fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/com/xsis/android/batch217/ui/back_office_position/BackOfficePositionFragment.kt
+++ app/src/main/java/com/xsis/android/batch217/ui/back_office_position/BackOfficePositionFragment.kt
@@ -8,25 +8,51 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.tabs.TabLayout
 import com.xsis.android.batch217.ui.back_office_position.BackOfficePositionViewModel
 import com.xsis.android.batch217.R
+import com.xsis.android.batch217.adapters.fragments.BackOfficeFragmentAdapter
+import com.xsis.android.batch217.utils.CustomViewPager
+import com.xsis.android.batch217.utils.OnBackPressedListener
 
-class BackOfficePositionFragment : Fragment() {
-
-    private lateinit var backOfficePositionViewModel: BackOfficePositionViewModel
+class BackOfficePositionFragment : Fragment(), OnBackPressedListener {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        backOfficePositionViewModel =
-            ViewModelProviders.of(this).get(BackOfficePositionViewModel::class.java)
+//        companyViewModel =
+//            ViewModelProviders.of(this).get(CompanyViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_back_office_position, container, false)
-        val textView: TextView = root.findViewById(R.id.text_back_office_position)
-        backOfficePositionViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        activity!!.title = getString(R.string.menu_back_office_position)
+
+        val fragmentAdapter = BackOfficeFragmentAdapter(context!!, childFragmentManager)
+        val viewPager = root.findViewById(R.id.viewPagerBackOffice) as CustomViewPager
+
+        //tambah tab di atas fragment
+        viewPager.adapter = fragmentAdapter
+
+        //nonaktifkan slide
+        viewPager.setSwipePagingEnabled(false)
+
+        val slidingTabs = root.findViewById(R.id.slidingTabsBackOffice) as TabLayout
+        slidingTabs.setupWithViewPager(viewPager)
+
+        //nonaktifkan klik tab
+        slidingTabs.touchables.forEach { view -> view.isEnabled = false }
+
         return root
+    }
+
+    override fun onBackPressed(): Boolean {
+        val viewPager = view!!.findViewById(R.id.viewPagerBackOffice) as CustomViewPager
+        if (viewPager.currentItem !=0) {
+            viewPager.setCurrentItem(0, true)
+            return true
+        }
+        return false
     }
 }
