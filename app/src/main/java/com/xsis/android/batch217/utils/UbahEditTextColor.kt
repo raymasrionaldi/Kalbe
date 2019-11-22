6b9124152803b223de0a764e1f096cfdaae8a62f
Erdyanti Rinta Bi Tari <rintabt@gmail.com>
2019-11-22 13:27:51 +0700

    UbahTextInputEditTextColor

--- app/src/main/java/com/xsis/android/batch217/utils/UbahEditTextColor.kt
+++ app/src/main/java/com/xsis/android/batch217/utils/UbahEditTextColor.kt
@@ -5,6 +5,8 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.widget.EditText
 import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.xsis.android.batch217.R
 
 fun ubahEditTextColor(context:Context, editText: EditText, empty:Boolean){
@@ -13,5 +15,16 @@ fun ubahEditTextColor(context:Context, editText: EditText, empty:Boolean){
         editText.backgroundTintList = ColorStateList.valueOf(Color.RED)
     } else{
         editText.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.warnaDetailText))
+    }
+}
+
+fun ubahTextInputEditTextColor(context:Context, editText: TextInputEditText, textInputLayout: TextInputLayout, empty:Boolean){
+    if (empty){
+        editText.setHintTextColor(Color.RED)
+        editText.backgroundTintList = ColorStateList.valueOf(Color.RED)
+        textInputLayout.setHintTextAppearance(R.style.TextAppearance_App_TextInputLayout)
+    } else{
+        editText.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.warnaDetailText))
+        textInputLayout.setHintTextAppearance(R.style.TextAppearance_App_TextInputLayoutBlack)
     }
 }
